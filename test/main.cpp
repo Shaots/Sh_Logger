6d21a8aa27faf421cbdfca8eb3c11f0ff94c5f38
shaots <shaots@mail.ru>
2024-11-09 21:30:38 +0300

    Fix test

--- test/main.cpp
+++ test/main.cpp
@@ -13,8 +13,15 @@ int main() {
 
 void test1() {
     ILogger* logger = ILogger::createLogger("log.txt");
-    logger->info(RC::SUCCESS);
     logger->log(RC::ALLOCATION_ERROR, ILogger::Level::SEVERE);
     logger->log(RC::IO_ERROR, ILogger::Level::WARNING, __FILE__, __FUNCTION__, __LINE__);
+
+    logger->info(RC::SUCCESS);
+    logger->warning(RC::MISMATCHING_DIMENSIONS);
+    logger->severe(RC::INFINITY_OVERFLOW);
+    logger->info(RC::UNKNOWN, __FILE__, __FUNCTION__, __LINE__);
+    logger->severe(RC::MEMORY_INTERSECTION, __FILE__, __FUNCTION__, __LINE__);
+    logger->warning(RC::INDEX_OUT_OF_BOUND, __FILE__, __FUNCTION__, __LINE__);
+
     delete logger;
 }
